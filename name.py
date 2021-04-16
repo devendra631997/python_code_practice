048d04064e02f320a9b4f4619373f4fb3dd34800
shaileshpatil1337 <51418310+shaileshpatil1337@users.noreply.github.com>
2021-04-16 11:13:16 +0530

    new
    unable to push files via command line

---
+++ name.py
@@ -0,0 +1,11 @@
+name = "Krrishna"
+surname = "patil"
+print(surname + " " + name)
+# Output patil krrishna
+
+
+
+name = "kudos"
+work = "finance"
+print( name + " " + work)
+# output kudos finance
